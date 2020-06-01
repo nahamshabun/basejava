92136b97e6bd188f07eccaed3f3558ff349771e6
Denis Stepanov <nahamshabun@gmail.com>
2020-06-01 21:31:03 +0300

    Add ArrayStorage methods implementation and the rest of provided classes

---
+++ src/ArrayStorage.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int storageSize = 0;
+
+    void clear() {
+        storage = null;
+        storageSize = 0;
+    }
+
+    void save(Resume r) {
+        storage[storageSize] = r;
+        storageSize++;
+    }
+
+    Resume get(String uuid) {
+        return storage[getResumeIndex(uuid)];
+    }
+
+    void delete(String uuid) {
+        storageSize--;
+        for (int i = getResumeIndex(uuid); i < storageSize; i++) {
+            storage[i] = storage[i + 1];
+        }
+        storage[storageSize] = null;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        if (storageSize == 0) {
+            return new Resume[0];
+        }
+        return Arrays.copyOfRange(storage, 0, storageSize);
+    }
+
+    int size() {
+        return storageSize;
+    }
+
+    private int getResumeIndex(String uuid) {
+        int resumeIndex = 0;
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resumeIndex = i;
+                break;
+            }
+        }
+        return resumeIndex;
+    }
+}
